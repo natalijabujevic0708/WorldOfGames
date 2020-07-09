1877a1228ce4aa158dfe345f893626b08cf0c1dd
natalijabujevic0708 <natalija.bujevic@gmail.com>
2020-07-09 16:09:23 +0000

    Changed the video in the header, adeed new background colours to the index.html, adeed a dot-nav for different descriptions of the games, created a click event in script.js for the nav dots

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-    
+    $('#dotGuess').click(function(){
+        $('#dotMemory').removeClass('activeDot');
+        $(this).addClass('activeDot');
+        $(".text h2").text("Guess the number")
+        $(".text p").text("Get into the head of the computer and guess what number he is thinking of!")
+    });
+    $('#dotMemory').click(function(){
+        $('#dotGuess').removeClass('activeDot');
+        $(this).addClass('activeDot');
+        $(".text h2").text("Memory game")
+        $(".text p").text("Test your memory with this fun game!")
+    });
 });
