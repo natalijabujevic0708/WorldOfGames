8e01ca61fc92041d5fdd73be2a410f0caa8c2f76
natalijabujevic0708 <natalija.bujevic@gmail.com>
2020-07-27 22:22:28 +0000

    Changed the function to correctly display top 5 high scores in the Guess Number game, styled it in style.css

--- assets/js/GuessNumberScript.js
+++ assets/js/GuessNumberScript.js
@@ -1,26 +1,27 @@
+let data = [];
 $(document).ready(function() {
     // guess the number game
     function radomNumber() {
         return computerGuess = Math.floor(Math.random()*501); // create a random number
     };
-    let data = []
+    $(document).ready(function(){
+        radomNumber();
+        return countTries=0;
+    });
     function addScore() {
+        data = JSON.parse(localStorage.getItem("scoreboard"));
+        let userName = prompt("Please enter your user name:");
         // Add the new score to end of 'data'
         data.push([countTries, userName]);
+    
         // Sort data by all of the scores
-       data.sort(function(a, b) {
-		    return a[0] < b[0];
-		});
-        // Take just the top 5 elements
-        if (data.length > 5) {
-            data = data[0, 5];
+        data.sort((a,b) => a[0] - b[0]);
+        
+        if (data.length > 5){
+            data = data.slice(0,5)
+        }
+        localStorage.setItem('scoreboard', JSON.stringify(data));
         };
-        return data;
-        };
-
-	function saveScores() {
-		localStorage.setItem('scoreboard', data);
-	}
 
     function guessNumber() {
         let userGuess = document.getElementById("userGuess").value;  // get the users guess
@@ -33,18 +34,30 @@ $(document).ready(function() {
             $("#instructions").html(`${userGuess} is incorrect <br> You aimed TOO HIGH <br> Try again!`);
         } else {
             addScore();
-            saveScores();
             $("#guessPage").html(`
                 <div class="congratulations">
                     <h2>Congratulations!!</h2>
                     <p>${userGuess} is correct! You can read the computer's mind. It took you ${countTries} tries.</p>
-                    <p id="highScore"></p>
+                    <h2> TOP 5 PLAYERS </h2>
+                    <div id="highScore"></div>
                     <button type="button" class="btn btn-secondary buttons"><a id="buttonPlay" href="GuessNumber.html">Play Again!</a></button>
                 </div> 
             `);
-             $("#highScore").text(localStorage.getItem('scoreboard'));
+            makeHighScoreTable();
+             
         };                                           
     }; 
+
+    function makeHighScoreTable() {
+        let arrayofScores = JSON.parse(localStorage.getItem("scoreboard"))
+        console.log(arrayofScores)
+        var content = "<table class='tableHighScores'><tr><td> Place </td><td> Count of Guesses</td><td>Username</td></tr>"
+        for(let i=0; i<5; i++){
+            content += '<tr><td>' + (i+1) + '</td><td>' + arrayofScores[i][0] + '</td><td>' + arrayofScores[i][1] +'</td></tr>';
+        };
+        content += "</table>"
+        $('#highScore').html(content);
+    }
 
     function NewUserGuess(){
         guessNumber();
@@ -60,11 +73,5 @@ $(document).ready(function() {
         NewUserGuess();
     });
 
-    $(document).ready(function(){
-        let name, userName
-        name = prompt("Please enter your user name:");
-        userName = name.value;
-        radomNumber();
-        return countTries=0, userName;
-    });
+   
 });
