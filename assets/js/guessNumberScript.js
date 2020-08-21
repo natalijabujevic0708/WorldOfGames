f44db16585a8f9871c18ea83a113411f4b5662bf
natalijabujevic0708 <natalija.bujevic@gmail.com>
2020-08-21 17:01:34 +0000

    Added placeholder tables for high score, changed the header elements in memory.html and guessNumber.html so it's more user-friendly

--- assets/js/guessNumberScript.js
+++ assets/js/guessNumberScript.js
@@ -9,14 +9,18 @@ $(document).ready(function() {
     });
     function guessNumber() {
         let userGuess = document.getElementById("userGuess").value;  // get the users guess
+        $( "#introductionGuess" ).remove();
         document.getElementById('userGuess').value = ""; // make the input field empty
         if (userGuess<1 || userGuess>500) {
             $("#instructions").text("Please enter a number between 1 and 500");
         }else if (userGuess<computerGuess) {
             $("#instructions").html(`${userGuess} is incorrect <br> You aimed TOO LOW <br> Try again!`);
+            countTries++;
         }else if (userGuess>computerGuess) {
             $("#instructions").html(`${userGuess} is incorrect <br> You aimed TOO HIGH <br> Try again!`);
+            countTries++;
         } else {
+            countTries++;
             localStorage.setItem('countTries', countTries);
             addScore("guessNumber");
             $("#guessPage").html(`
@@ -35,7 +39,6 @@ $(document).ready(function() {
 
     function NewUserGuess(){
         guessNumber();
-        countTries++;
     }
     window.addEventListener('keypress', function (e) {
         if (e.key === 'Enter') {
